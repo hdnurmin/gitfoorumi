081fac964d32ac1f4ac48025acf63095822244ae
hdnurmin <heidi.a.nurminen@gmail.com>
2018-03-13 11:17:53 +0200

    Projekti luotu

---
+++ src/main/java/fi/academy/foorumi/FoorumiApplication.java
@@ -0,0 +1,12 @@
+package fi.academy.foorumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoorumiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoorumiApplication.class, args);
+	}
+}
