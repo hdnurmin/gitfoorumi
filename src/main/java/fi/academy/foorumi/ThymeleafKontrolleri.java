97b4588d9bbfe8ec263aef4bb0cdde467c7e2ade
hdnurmin <heidi.a.nurminen@gmail.com>
2018-03-13 13:42:10 +0200

    Thymeleafkontrolleriin lisätty aihealueen listaus etusivulle, etusivulle lisätty viittaukset thymeleafkontrolleriin

--- src/main/java/fi/academy/foorumi/ThymeleafKontrolleri.java
+++ src/main/java/fi/academy/foorumi/ThymeleafKontrolleri.java
@@ -1,8 +1,19 @@
 package fi.academy.foorumi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ThymeleafKontrolleri {
-    
+
+    @Autowired
+    private OtsikkoRepositorio otsikkorepo;
+
+    @GetMapping ("/etusivu")
+    public String aihelistausEtusivulle (Model model) {
+        model.addAttribute("otsikko", otsikkorepo.findAll());
+        return "etusivu";
+    }
 }
